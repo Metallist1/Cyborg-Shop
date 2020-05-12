68e98100949054e489cbc2bc8e5726061e63b15f
Nedas <nedassurkus@gmail.com>
2020-05-12 05:09:31 +0200

    Created order

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBanC5pKQnSHD8tf4lDkDJcEXlbKNaSGKk',
-    authDomain: 'web-dev-exam-71d64.firebaseapp.com',
-    databaseURL: 'https://web-dev-exam-71d64.firebaseio.com',
-    projectId: 'web-dev-exam-71d64',
-    storageBucket: 'web-dev-exam-71d64.appspot.com',
-    messagingSenderId: '906484381716',
-    appId: '1:906484381716:web:b780d7a6f2afe1de818f01'
-  }
+
 };
 
 /*
